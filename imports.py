1bf9ff9c3563bfcca9fa90ceae50f9935d97c13f
Kevin Ross <r0ssar00@gmail.com>
2012-11-13 17:02:20 -0500

    static resources during devel should be served by app

--- imports.py
+++ imports.py
@@ -3,6 +3,8 @@ import bottle, re, os, simplejson
 from i18n import i18n
 import calendar, datetime as datetime
 def static(path):
+	if bottle.request.environ.get('MODE','devel') == 'devel':
+		return path
 	host_parts = bottle.request.environ['HTTP_HOST'].split('.')
 	host = '.'.join(['static',host_parts[-2],host_parts[-1]])
 	return '//%s/%s' % (host, path.lstrip('/'))
