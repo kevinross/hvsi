5daa5964f9655d7dd990ac8c80071c7a041559af
Kevin Ross <r0ssar00@gmail.com>
2013-01-15 01:26:55 -0500

    let the app attempt to load config from the environment before falling back to the config app

--- settings.py
+++ settings.py
@@ -1,7 +1,36 @@
-import os, sys, urlimport
+import os
 # SETTINGS could be a URL or the name of the module in the tree
-# if it's a url, urlimport will import it.  If not, standard machinery will import it
-base = os.path.dirname(os.environ.get('SETTINGS', ''))
-mod = os.path.basename(os.environ.get('SETTINGS', 'instanceconfig')).replace('.py','')
+# if it's a url]
+base = os.path.dirname(os.environ['SETTINGS'])
+mod = os.path.basename(os.environ['SETTINGS'])
 sys.path.append(base)
-instanceconfig = __import__(mod)
+try:
+	instanceconfig = __import__(mod)
+except:
+	# last resort: use environment variables to configure the app.  Does not use defaults so
+	# config_app will run if one of the required values isn't present
+	from module_creator import importCode
+	code = """
+host		= %r
+statichost	= %r
+debug		= %r
+timezone	= %r
+dbprot		= %r
+dbhost		= %r
+dbport		= %r
+dbuser		= %r
+dbpass		= %r
+dbdb		= %r
+	""" % (
+		os.environ['APP_HOST'],
+		os.environ['APP_STATICHOST'],
+		True if os.environ['APP_DEBUG'] == 'True' else False,
+		os.environ['APP_TIMEZONE'],
+		os.environ['APP_DBPROTO'],
+		os.environ['APP_DBHOST'],
+		int(os.environ['APP_DBPORT']),
+		os.environ['APP_DBUSER'],
+		os.environ['APP_DBPASS'],
+		os.environ['APP_DB']
+	)
+	instanceconfig = importCode(code, 'instanceconfig')
